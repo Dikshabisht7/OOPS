58ae7e21738af97d877edef67840ac45807162d9
Dikshabisht7 <54893688+Dikshabisht7@users.noreply.github.com>
2020-08-01 23:35:17 +0530

    Program to print diamond

---
+++ Assignment/Assignment2/Q1diamond.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,n;
+    cout<<"Enter number of rows:";
+    cin>>n;
+    for(i=1;i<=n;i++)
+    {
+        for(j=n;j>i;j--)
+        {
+            cout<<" ";
+        }
+        cout<<"*";
+        for(j=1;j<(i-1)*2;j++)
+        {
+            cout<<" ";
+        }
+        if(i==1)
+        {
+            cout<<"\n";
+        }
+        else
+        {
+            cout<<"*\n";
+        }
+    }
+    for(i=n-1;i>=1;i--)
+    {
+        for(j=n;j>i;j--)
+        {
+            cout<<" ";
+        }
+        cout<<"*";
+        for(j=1;j<(i-1)*2;j++)
+        {
+            cout<<" ";
+        }
+        if(i==1)
+        {
+            cout<<"\n";
+        }
+        else
+            cout<<"*\n";
+    }
+}
